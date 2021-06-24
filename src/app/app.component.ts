282a7663b6202bbccfda393aba443e26c78a0971
werner <elmer@werners-MacBook-Air.local>
2021-06-24 01:25:15 -0700

    Finalized angular challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  elmer ={
+    name: "elmer",
+    age: "19",
+    text: "This text should be altered with two way data binding."
+  }
   title = 'angularChallenge';
 }
